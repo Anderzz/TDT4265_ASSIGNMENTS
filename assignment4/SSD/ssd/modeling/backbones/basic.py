cfbebf9e34d45973787824dd4af2f65249655045
Anders Kjelsrud <andekje@clab13.idi.ntnu.no>
2023-03-16 11:57:50 +0100

    start ssd

--- assignment4/SSD/ssd/modeling/backbones/basic.py
+++ assignment4/SSD/ssd/modeling/backbones/basic.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from typing import Tuple, List
 
 
@@ -13,13 +14,30 @@ class BasicModel(torch.nn.Module):
      shape(-1, output_channels[3], 3, 3),
      shape(-1, output_channels[4], 1, 1)]
     """
+
     def __init__(self,
-            output_channels: List[int],
-            image_channels: int,
-            output_feature_sizes: List[Tuple[int]]):
+                 output_channels: List[int],
+                 image_channels: int,
+                 output_feature_sizes: List[Tuple[int]]):
         super().__init__()
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
+
+        self.feature_extractor = nn.ModuleList([
+            nn.Sequential(
+                nn.Conv2d(512, 1024, kernel_size=3, padding=1, stride=1),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2, padding=1, dilation=1),
+                nn.Conv2d(1024, 1024, kernel_size=1),
+                nn.ReLU(),
+            ),
+            nn.Sequential(
+                nn.Conv2d(1024, 256, kernel_size=1),
+                nn.ReLU(),
+                nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
+                nn.ReLU(),
+            ),
+        ])
 
     def forward(self, x):
         """
@@ -44,4 +62,3 @@ class BasicModel(torch.nn.Module):
         assert len(out_features) == len(self.output_feature_shape),\
             f"Expected that the length of the outputted features to be: {len(self.output_feature_shape)}, but it was: {len(out_features)}"
         return tuple(out_features)
-
